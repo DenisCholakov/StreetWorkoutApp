0feacb1c359610f19803d46d4fec3c9f9d654d5d
Denis Cholakov <dencho@cobuilder.no>
2021-07-12 15:19:55 +0300

    [front-end]change routes for thw web api

--- StreetWorkoutClientApp/src/app/services/auth.service.ts
+++ StreetWorkoutClientApp/src/app/services/auth.service.ts
@@ -9,13 +9,15 @@ import { IUserLoginForm, IUserRegisterForm } from '../models';
   providedIn: 'root',
 })
 export class AuthService {
-  private loginPath = environment.apiUrl + 'login';
+  private loginPath = environment.apiUrl + 'identity/login';
   private registerPath = environment.apiUrl + 'register';
 
   constructor(private http: HttpClient) {}
 
   login(data: IUserLoginForm): Observable<any> {
-    return this.http.post(this.loginPath, data);
+    return this.http.post(this.loginPath, data, {
+      headers: { 'content-type': 'application/json' },
+    });
   }
 
   register(data: IUserRegisterForm): Observable<any> {
